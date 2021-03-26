d48819c731e21ed31d607b6cadced2650e417ddd
Ingrid Vargas Badilla <invabadilla@estudiantec.cr>
2021-03-26 12:16:59 -0600

    a

---
+++ pruebita/cmake-build-debug/CMakeFiles/pruebita.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pruebita.dir/main.cpp.obj"
+  "libpruebita.dll.a"
+  "pruebita.exe"
+  "pruebita.exe.manifest"
+  "pruebita.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pruebita.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
